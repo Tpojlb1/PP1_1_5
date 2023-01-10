e8ce302f9479d748978092b7763f692df3182d3d
Tpojlb1 <118221543+Tpojlb1@users.noreply.github.com>
2023-01-10 14:28:40 +0700

    Update Main.java

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,5 +1,6 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.service.UserServiceImpl;
 
@@ -7,28 +8,18 @@ import java.util.List;
 
 public class Main {
 public static void main(String[] args) {
-    User userIvan = new User("Иван", "Иванов", (byte) 45);
-    User userAnton = new User("Антон", "Петров", (byte) 35);
-    User userBoris = new User("Борис", "Кузьмин", (byte) 45);
-    User userJake = new User("Джейк", "Смит", (byte) 55);
-
+    UserDaoHibernateImpl userDaoHibernate = new UserDaoHibernateImpl();
     UserServiceImpl userService = new UserServiceImpl();
-
-    userService.createUsersTable();
-
-    userService.saveUser(userIvan.getName(), userIvan.getLastName(), userIvan.getAge());
-    userService.saveUser(userAnton.getName(), userAnton.getLastName(), userAnton.getAge());
-    userService.saveUser(userBoris.getName(), userBoris.getLastName(), userBoris.getAge());
-    userService.saveUser(userJake.getName(), userJake.getLastName(), userJake.getAge());
-
-    List<User> userList = userService.getAllUsers();
-
-    for (User us : userList) {
-        System.out.println(us.toString());
-    }
+    userDaoHibernate.createUsersTable();
+    userDaoHibernate.saveUser("Иван", "Иванов", (byte) 45);
+    userDaoHibernate.saveUser("Антон", "Петров", (byte) 35);
+    userDaoHibernate.saveUser("Борис", "Кузьмин", (byte) 45);
+    userDaoHibernate.saveUser("Джейк", "Смит", (byte) 55);
+    System.out.println(userDaoHibernate.getAllUsers());
 
     userService.cleanUsersTable();
 
     userService.dropUsersTable();
 }
+
 }
