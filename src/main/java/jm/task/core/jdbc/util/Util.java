ea43120787e542f164f4f6cc65d4ff2b5ba6dd29
Tpojlb1 <118221543+Tpojlb1@users.noreply.github.com>
2023-01-10 14:26:59 +0700

    Update Util.java

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -3,40 +3,49 @@ package jm.task.core.jdbc.util;
 import jm.task.core.jdbc.model.User;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.AvailableSettings;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.cfg.Environment;
 import org.hibernate.service.ServiceRegistry;
-
 import java.util.Properties;
 
+
 public class Util {
+private static final String URL = "jdbc:mysql://localhost:3306/username";
+private static final String USER = "root";
+private static final String PASSWORD = "root";
+private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
 private static SessionFactory sessionFactory;
-private static final Environment environment = null;
 
 public static SessionFactory getSessionFactory() {
-    if (sessionFactory == null) {
-        try {
-            Configuration config = new Configuration();
-            Properties prop = new Properties();
-            prop.put(environment.DRIVER, "com.mysql.cj.jdbc.Driver");
-            prop.put(environment.URL, "jdbc:mysql://localhost:3306/username");
-            prop.put(environment.USER, "root");
-            prop.put(environment.PASS, "root");
-            prop.put(environment.DIALECT, "org.hibernate.dialect.MySQL8Dialect");
-            prop.put(environment.SHOW_SQL, "true");
-            prop.put(environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
 
-            config.setProperties(prop);
-            config.addAnnotatedClass(User.class);
+    if (sessionFactory == null) {
+
+        try {
+            Configuration configuration = new Configuration();
+
+            Properties properties = new Properties();
+            properties.put(Environment.URL, URL);
+            properties.put(Environment.USER, USER);
+            properties.put(Environment.PASS, PASSWORD);
+            properties.put(Environment.DRIVER, DRIVER);
+            properties.put(AvailableSettings.DIALECT, "org.hibernate.dialect.MySQLDialect");
+            properties.put(AvailableSettings.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+            properties.put(AvailableSettings.SHOW_SQL, "true");
+
+            configuration.setProperties(properties);
+            configuration.addAnnotatedClass(User.class);
 
             ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
-                    .applySettings(config.getProperties()).build();
-            sessionFactory = config.buildSessionFactory(serviceRegistry);
+                    .applySettings(configuration.getProperties())
+                    .build();
 
-        } catch (Throwable ex) {
-            System.err.println("Initial SessionFactory creation failed." + ex);
-            throw new ExceptionInInitializerError(ex);
+            sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+        } catch (Exception e) {
+            System.out.println("Check Util configuration!");
+            e.printStackTrace();
         }
+
     }
     return sessionFactory;
 }
